c8c4f02bd05b63166c97881519ecd82826cfe76f
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2024-02-17 10:56:52 -0300

    feat: implement unit tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { PrismaService } from '@utils/prisma/prisma.service';
+import { TeamsModule } from '@api/teams/teams.module';
+import { PlayersModule } from '@api/players/players.module';
+import { TransfersModule } from '@api/transfers/transfers.module';
 
 @Module({
-  imports: [],
+  imports: [TeamsModule, PlayersModule, TransfersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
